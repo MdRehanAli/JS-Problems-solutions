94597bfd854f8162ccef2336c85996fc03e4272a
Md Rehan Ali <rehanali21d@gmail.com>
2025-08-04 13:52:15 +0600

    Practices

---
+++ practice.js
@@ -0,0 +1,43 @@
+// problem 1:
+myMoney = 1000;
+applePrice = 400;
+orangePrice = 300;
+totalPrice = (applePrice + orangePrice);
+remainingBalance = (myMoney - totalPrice);
+console.log(remainingBalance);
+
+// problem 2:
+var Mathematics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+
+var totalSubject = 5;
+
+var totalMarks = (Mathematics + Biology + Chemistry + Physics + Bangla);
+var averageMarks = totalMarks / totalSubject;
+console.log(parseFloat(averageMarks.toFixed(2)));
+
+// Problem 3:
+num1 = 119;
+num2 = 5;
+remainder = num1 % num2;
+console.log(remainder);
+
+// Problem 4:
+const a = isNaN('11');
+console.log(a);
+const b = isNaN(2-11);
+console.log(b);
+
+
+
+// Day 2:
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
---
+++ problem2.js
@@ -0,0 +1,3 @@
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
