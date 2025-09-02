a8eb62a28d14e0478b035da924244f9962b21b67
Md Rehan Ali <rehanali21d@gmail.com>
2025-09-03 04:07:20 +0600

    Added New Problem

--- practice2.js
+++ practice2.js
@@ -88,3 +88,91 @@ As Ersa is learning now, she wants to explore more and more. Tell Ersa to genera
  */
 
 
+let n = 5;
+let i = 1;
+
+while (i <= 10) {
+    console.log(`${n} x ${i} = ${n * i}`);
+    i++;
+}
+
+
+
+/***
+
+Implement a countdown timer that counts down from 21 to 15.
+
+ */
+
+let num5 = 21;
+while (num5 >= 15) {
+    console.log(num5);
+    num5--;
+}
+
+
+/* 
+Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
+*/
+
+for (let i = 1; i <= 40; i++) {
+    if (i % 2 != 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+
+/*
+display odd number from 55 to 85 and skip the numbers divisible by 5.
+*/
+
+for (let i = 55; i <= 85; i++) {
+    if (i % 2 != 0) {
+        if (i % 5 == 0) {
+            continue;
+        }
+        console.log(i);
+    }
+}
+
+
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+
+
+for (let i = 1; i <= 200; i++) {
+    console.log(i);
+    if (i == 100) {
+        break;
+    }
+}
+
+
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+
+let sum3 = 0;
+for (let i = 1; ; i++) {
+    sum3 += i;
+    if (sum3 >= 100) {
+        break;
+    }
+    console.log(sum3);
+}
+
+
+/* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+
+for (let i = 1; i < 100; i++) {
+    if (i ** i <= 100) {
+        console.log("First Square root is: ", i * i);
+        break
+    }
+}
